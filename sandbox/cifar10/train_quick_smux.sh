b47596dcd47814a4bb35a383c58e6c563c63c6e2
Stephen Gould <sgould@woodside.cecs.anu.edu.au>
2016-06-03 18:11:21 +1000

    fixed script error

--- sandbox/cifar10/train_quick_smux.sh
+++ sandbox/cifar10/train_quick_smux.sh
@@ -6,15 +6,15 @@ TOOLS=./build/tools
 if [ $TRAIN != 0 ] 
 then
     $TOOLS/caffe train \
-        --solver=sandbox/cifar10/cifar10_quick_smux_solver.prototxt
+        --solver=sandbox/cifar10/cifar10_quick_smux_solver.prototxt || exit 1
 fi
 
 # reduce learning rate by factor of 10 after 8 epochs
-if [ $TRAIN != 0]
+if [ $TRAIN != 0 ]
 then
     $TOOLS/caffe train \
         --solver=sandbox/cifar10/cifar10_quick_smux_solver_lr1.prototxt \
-        --snapshot=sandbox/cifar10/cifar10_quick_smux_iter_4000.solverstate.h5
+        --snapshot=sandbox/cifar10/cifar10_quick_smux_iter_4000.solverstate.h5 || exit 1
 fi
 
 # just test
@@ -22,5 +22,5 @@ if [ $TRAIN == 0 ]
 then
     $TOOLS/caffe test \
         --model=sandbox/cifar10/cifar10_quick_smux_train_test.prototxt \
-        --weights=sandbox/cifar10/cifar10_quick_smux_iter_5000.caffemodel.h5
+        --weights=sandbox/cifar10/cifar10_quick_smux_iter_5000.caffemodel.h5 || exit 1
 fi
